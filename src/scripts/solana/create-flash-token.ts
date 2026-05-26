f57ad9d40e6f23f1bb78989bf1c5cbb0f73ab65d
dante-243 <simbioza.volley@gmail.com>
2026-05-26 22:45:11 +0200

    confirm token vanity on success and release it on failure

--- src/scripts/solana/create-flash-token.ts
+++ src/scripts/solana/create-flash-token.ts
@@ -8,192 +8,236 @@ import {
 } from 'schema/flash-token/solana/api';
 import { CreateSolanaFlashInput } from 'schema/flash-token/solana/sdk';
 import { SolanaFlashValidator } from 'schema/flash-token/solana/validator';
+import { SolanaChainId } from 'types/chain-id';
 import { BasedBidApi, IpfsUpload, SolanaWrapper } from 'utils';
 
+let launchedToken: {
+  chainId: SolanaChainId;
+  mintAddress: string;
+  signature: string;
+} | null = null;
+
 export const createFlashTokenSolana = async (args: CreateSolanaFlashInput) => {
-  const env = validateEnvironmentSolana();
+  try {
+    const env = validateEnvironmentSolana();
 
-  const input = SolanaFlashValidator.validateInput(args);
+    const input = SolanaFlashValidator.validateInput(args);
 
-  const solanaWrapper = new SolanaWrapper(
-    env.SOLANA_RPC_URL,
-    env.SOLANA_PRIVATE_KEY,
-  );
-  await solanaWrapper.init();
+    const solanaWrapper = new SolanaWrapper(
+      env.SOLANA_RPC_URL,
+      env.SOLANA_PRIVATE_KEY,
+    );
+    await solanaWrapper.init();
 
-  const logoUrl = await IpfsUpload.uploadImage(args.token.metadata.logo);
+    const logoUrl = await IpfsUpload.uploadImage(args.token.metadata.logo);
 
-  const data = input;
-  const { token, raydium, meteora, board, boardOwner } = data;
+    const data = input;
+    const { token, raydium, meteora, board, boardOwner } = data;
 
-  const metadataIpfs = {
-    name: token.name,
-    symbol: token.symbol,
-    decimals: token.decimals,
-    totalSupply: token.totalSupply,
-    logo: logoUrl,
-    twitter: token.metadata.twitter,
-    telegram: token.metadata.telegram,
-    website: token.metadata.website,
-    discord: token.metadata.discord,
-    description: token.metadata.description,
-    board,
-    boardOwner,
-  };
-
-  const metadataUrl = await IpfsUpload.uploadMetadata(metadataIpfs);
-
-  // Determine flashDex value (1 = Meteora, 2 = Raydium)
-  const flashDex = args.flashDex === SolanaDexType.METEORA ? 1 : 2;
-
-  // ==================== TX1 ====================
-  console.log('\n=== Preparing Transaction 1 ===');
-
-  const tx1Payload: CreateSolanaFlashTx1Api = {
-    chainId: args.chainId,
-    signer: solanaWrapper.publicKey,
-    flashDex,
-    isSandboxMode: args.isSandboxMode,
-    token: {
+    const metadataIpfs = {
       name: token.name,
       symbol: token.symbol,
-      metadataUrl: metadataUrl,
-      totalSupply: token.totalSupply,
       decimals: token.decimals,
-    },
-    // Add Raydium specific fields if applicable
-    ...(input.flashDex === SolanaDexType.RAYDIUM &&
-      raydium && {
-        raydiumFeeTierIndex: raydium.feeTierIndex,
-        finalStartPrice: raydium.finalStartPrice,
-        hasInitialSwap: raydium.hasInitialSwap,
-        solanaInitialBuyHuman: raydium.solanaInitialBuyHuman,
-      }),
-    // Add Meteora specific fields if applicable
-    ...(input.flashDex === SolanaDexType.METEORA &&
-      meteora && {
-        baseTokenMint: 'So11111111111111111111111111111111111111112',
-        virtualUsd: meteora.virtualUsd,
-        nativeSolPriceUsd: meteora.nativeSolPriceUsd,
-        meteoraFeeTierIndex: meteora.feeTierIndex,
-        hasHookDynamicFee: meteora.hasHookDynamicFee,
-        boardSeed: meteora.boardSeed,
-      }),
-  };
+      totalSupply: token.totalSupply,
+      logo: logoUrl,
+      twitter: token.metadata.twitter,
+      telegram: token.metadata.telegram,
+      website: token.metadata.website,
+      discord: token.metadata.discord,
+      description: token.metadata.description,
+      board,
+      boardOwner,
+    };
 
-  console.log('Calling API for Transaction 1...');
-  const tx1Response =
-    await BasedBidApi.invokeApi<CreateSolanaFlashTx1ApiResponse>(
+    const metadataUrl = await IpfsUpload.uploadMetadata(metadataIpfs);
+
+    // Determine flashDex value (1 = Meteora, 2 = Raydium)
+    const flashDex = args.flashDex === SolanaDexType.METEORA ? 1 : 2;
+
+    // ==================== TX1 ====================
+    console.log('\n=== Preparing Transaction 1 ===');
+
+    const tx1Payload: CreateSolanaFlashTx1Api = {
+      chainId: args.chainId,
+      signer: solanaWrapper.publicKey,
+      flashDex,
+      token: {
+        name: token.name,
+        symbol: token.symbol,
+        metadataUrl: metadataUrl,
+        totalSupply: token.totalSupply,
+        decimals: token.decimals,
+      },
+      // Add Raydium specific fields if applicable
+      ...(input.flashDex === SolanaDexType.RAYDIUM &&
+        raydium && {
+          raydiumFeeTierIndex: raydium.feeTierIndex,
+          finalStartPrice: raydium.finalStartPrice,
+          hasInitialSwap: raydium.hasInitialSwap,
+          solanaInitialBuyHuman: raydium.solanaInitialBuyHuman,
+        }),
+      // Add Meteora specific fields if applicable
+      ...(input.flashDex === SolanaDexType.METEORA &&
+        meteora && {
+          baseTokenMint: 'So11111111111111111111111111111111111111112',
+          virtualUsd: meteora.virtualUsd,
+          nativeSolPriceUsd: meteora.nativeSolPriceUsd,
+          meteoraFeeTierIndex: meteora.feeTierIndex,
+          hasHookDynamicFee: meteora.hasHookDynamicFee,
+          boardSeed: meteora.boardSeed,
+        }),
+    };
+
+    console.log('Calling API for Transaction 1...');
+
+    const tx1Response =
+      await BasedBidApi.invokeApi<CreateSolanaFlashTx1ApiResponse>(
+        ApiType.SDK,
+        'sol/create-flash-tx1',
+        tx1Payload,
+        'Failed to create flash token Transaction 1',
+        args.isSandboxMode,
+      );
+
+    if (!tx1Response || !tx1Response.transaction) {
+      throw new Error(
+        'Based Bid API Error: Failed to create flash LBP Transaction 1',
+      );
+    }
+
+    const mintSigner = await solanaWrapper.getSignerFromPrivateKey(
+      tx1Response.mintSignerSecretHex,
+    );
+
+    const tx1Signature = await solanaWrapper.sendTransaction(
+      tx1Response.transaction,
+      tx1Response.blockhash,
+      tx1Response.lastValidBlockHeight,
+      [mintSigner.keyPair],
+      {
+        description: 'Create Flash Token (TX1)',
+        skipConfirmation: args.isSandboxMode,
+      },
+    );
+
+    launchedToken = {
+      chainId: args.chainId,
+      mintAddress: tx1Response.mintAddress,
+      signature: tx1Signature,
+    };
+
+    await solanaWrapper.awaitTxConfirmation(tx1Signature);
+
+    // ==================== TX2 ====================
+    console.log('\n=== Preparing Transaction 2 ===');
+
+    const tx2Payload: CreateSolanaFlashTx2Api = {
+      chainId: args.chainId,
+      signer: solanaWrapper.publicKey,
+      flashDex,
+      tx1Signature,
+      flashSeed: tx1Response.flashSeed,
+      mintAddress: tx1Response.mintAddress,
+      baseTokenMint: 'So11111111111111111111111111111111111111112',
+      raiseTokenDecimals: 9,
+      token: {
+        totalSupply: args.token.totalSupply,
+        decimals: 9,
+      },
+      // Add Raydium specific fields if applicable
+      ...(args.flashDex === SolanaDexType.RAYDIUM &&
+        args.raydium && {
+          raydiumFeeTierIndex: '0',
+          finalStartPrice: 100,
+          hasInitialSwap: false,
+        }),
+      // Add Meteora specific fields if applicable
+      ...(args.flashDex === SolanaDexType.METEORA &&
+        args.meteora && {
+          meteoraFeeTierIndex: args.meteora.feeTierIndex,
+          meteoraTokenAccountSeed: tx1Response.meteoraTokenAccountSeed,
+        }),
+    };
+
+    console.log('Calling API for Transaction 2...');
+    const tx2Response =
+      await BasedBidApi.invokeApi<CreateSolanaFlashTx1ApiResponse>(
+        ApiType.SDK,
+        'sol/create-flash-tx2',
+        tx2Payload,
+        'Failed to create flash token Transaction 2',
+        args.isSandboxMode,
+      );
+
+    if (!tx2Response || !tx2Response.transaction) {
+      throw new Error(
+        'Based Bid API Error: Failed to create flash LBP Transaction 2',
+      );
+    }
+
+    console.log('Transaction 2 data received');
+
+    const tx2Signers = [];
+
+    // For Raydium TX2 (and potentially others), there's a position NFT mint keypair signer
+    if (tx2Response.positionNftSignerSecretHex) {
+      const tx2PositionNftSigner = await solanaWrapper.getSignerFromPrivateKey(
+        tx2Response.positionNftSignerSecretHex,
+      );
+
+      tx2Signers.push(tx2PositionNftSigner.keyPair);
+    }
+
+    const tx2Signature = await solanaWrapper.sendTransaction(
+      tx2Response.transaction,
+      tx2Response.blockhash,
+      tx2Response.lastValidBlockHeight,
+      tx2Signers,
+      {
+        description: 'Create Flash Token (TX2)',
+        skipConfirmation: args.isSandboxMode,
+      },
+    );
+
+    await solanaWrapper.awaitTxConfirmation(tx2Signature);
+
+    await BasedBidApi.invokeApi(
       ApiType.SDK,
-      'sol/create-flash-tx1',
-      tx1Payload,
-      'Failed to create flash token Transaction 1',
+      'sol/confirm-launch',
+      {
+        chainId: args.chainId,
+        mintAddress: tx1Response.mintAddress,
+        signature: tx1Signature,
+      },
+      'Failed to confirm launch',
       args.isSandboxMode,
     );
 
-  if (!tx1Response || !tx1Response.transaction) {
-    throw new Error(
-      'Based Bid API Error: Failed to create flash LBP Transaction 1',
-    );
+    return {
+      mintAddress: tx1Response.mintAddress,
+      tx1Signature,
+      tx2Signature,
+      metadataUrl,
+      meteoraTokenAccountSeed: tx1Response.meteoraTokenAccountSeed,
+      positionNftMintAddress: tx1Response.positionNftMintAddress,
+    };
+  } catch (error) {
+    console.error('Error creating Solana flash token: ', error);
+
+    if (launchedToken != null) {
+      await BasedBidApi.invokeApi(
+        ApiType.SDK,
+        'sol/release-vanity',
+        {
+          chainId: launchedToken.chainId,
+          mintAddress: launchedToken.mintAddress,
+          signature: launchedToken.signature,
+        },
+        'Failed to release vanity',
+        args.isSandboxMode,
+      );
+    }
+
+    throw error;
   }
-
-  const mintSigner = await solanaWrapper.getSignerFromPrivateKey(
-    tx1Response.mintSignerSecretHex,
-  );
-
-  const tx1Signature = await solanaWrapper.sendTransaction(
-    tx1Response.transaction,
-    tx1Response.blockhash,
-    tx1Response.lastValidBlockHeight,
-    [mintSigner.keyPair],
-    {
-      description: 'Create Flash Token (TX1)',
-      skipConfirmation: args.isSandboxMode,
-    },
-  );
-
-  await solanaWrapper.awaitTxConfirmation(tx1Signature);
-
-  // ==================== TX2 ====================
-  console.log('\n=== Preparing Transaction 2 ===');
-
-  const tx2Payload: CreateSolanaFlashTx2Api = {
-    chainId: 5011,
-    signer: solanaWrapper.publicKey,
-    flashDex,
-    isSandboxMode: args.isSandboxMode,
-    tx1Signature,
-    flashSeed: tx1Response.flashSeed,
-    mintAddress: tx1Response.mintAddress,
-    baseTokenMint: 'So11111111111111111111111111111111111111112',
-    raiseTokenDecimals: 9,
-    token: {
-      totalSupply: args.token.totalSupply,
-      decimals: 9,
-    },
-    // Add Raydium specific fields if applicable
-    ...(args.flashDex === SolanaDexType.RAYDIUM &&
-      args.raydium && {
-        raydiumFeeTierIndex: '0',
-        finalStartPrice: 100,
-        hasInitialSwap: false,
-      }),
-    // Add Meteora specific fields if applicable
-    ...(args.flashDex === SolanaDexType.METEORA &&
-      args.meteora && {
-        meteoraFeeTierIndex: args.meteora.feeTierIndex,
-        meteoraTokenAccountSeed: tx1Response.meteoraTokenAccountSeed,
-      }),
-  };
-
-  console.log('Calling API for Transaction 2...');
-  const tx2Response =
-    await BasedBidApi.invokeApi<CreateSolanaFlashTx1ApiResponse>(
-      ApiType.SDK,
-      'sol/create-flash-tx2',
-      tx2Payload,
-      'Failed to create flash token Transaction 2',
-      args.isSandboxMode,
-    );
-
-  if (!tx2Response || !tx2Response.transaction) {
-    throw new Error(
-      'Based Bid API Error: Failed to create flash LBP Transaction 2',
-    );
-  }
-
-  console.log('Transaction 2 data received');
-
-  const tx2Signers = [];
-
-  // For Raydium TX2 (and potentially others), there's a position NFT mint keypair signer
-  if (tx2Response.positionNftSignerSecretHex) {
-    const tx2PositionNftSigner = await solanaWrapper.getSignerFromPrivateKey(
-      tx2Response.positionNftSignerSecretHex,
-    );
-
-    tx2Signers.push(tx2PositionNftSigner.keyPair);
-  }
-
-  const tx2Signature = await solanaWrapper.sendTransaction(
-    tx2Response.transaction,
-    tx2Response.blockhash,
-    tx2Response.lastValidBlockHeight,
-    tx2Signers,
-    {
-      description: 'Create Flash Token (TX2)',
-      skipConfirmation: args.isSandboxMode,
-    },
-  );
-
-  await solanaWrapper.awaitTxConfirmation(tx2Signature);
-
-  return {
-    mintAddress: tx1Response.mintAddress,
-    tx1Signature,
-    tx2Signature,
-    metadataUrl,
-    meteoraTokenAccountSeed: tx1Response.meteoraTokenAccountSeed,
-    positionNftMintAddress: tx1Response.positionNftMintAddress,
-  };
 };
--- src/scripts/solana/create-lbp.ts
+++ src/scripts/solana/create-lbp.ts
@@ -7,6 +7,7 @@ import { createLbpSolanaApiPayloadSchema } from 'schema/lbp/solana/api-request';
 import { solanaFeeDistributionApiPayloadSchema } from 'schema/lbp/solana/fee-distribution';
 import { CreateSolanaLbpInput } from 'schema/lbp/solana/sdk-input';
 import { SolanaLbpValidator } from 'schema/lbp/solana/validator';
+import { SolanaChainId } from 'types/chain-id';
 import {
   BasedBidApi,
   getLaunchPackageIndex,
@@ -15,153 +16,199 @@ import {
   SolanaWrapper,
 } from 'utils';
 
+let launchedToken: {
+  chainId: SolanaChainId;
+  mintAddress: string;
+  signature: string;
+} | null = null;
+
 export const createLbpSolana = async (args: CreateSolanaLbpInput) => {
-  const env = validateEnvironmentSolana();
+  try {
+    const env = validateEnvironmentSolana();
 
-  const input = SolanaLbpValidator.validateInput(args);
+    const input = SolanaLbpValidator.validateInput(args);
 
-  const solanaWrapper = new SolanaWrapper(
-    env.SOLANA_RPC_URL,
-    env.SOLANA_PRIVATE_KEY,
-  );
-  await solanaWrapper.init();
-
-  const data = input;
-  const { token, board, boardOwner, dex, fees } = data;
-
-  let sale = data.sale;
-
-  if (!sale) {
-    sale = {
-      marketCap: '9000',
-      maxAllocationPerUser: '0',
-      whitelistedAddresses: [],
-    };
-  }
-
-  const logoUrl = await IpfsUpload.uploadImage(token.metadata.logo);
-
-  const seed = SeedGenerator.generateNumericSeed(5);
-
-  const ipfsMetadata = {
-    name: token.name,
-    symbol: token.symbol,
-    decimals: SOLANA_DECIMALS,
-    totalSupply: token.totalSupply,
-    logo: logoUrl,
-    twitter: token.metadata.twitter,
-    telegram: token.metadata.telegram,
-    website: token.metadata.website,
-    discord: token.metadata.discord,
-    description: token.metadata.description,
-    whitelist: sale.whitelistedAddresses,
-    board,
-    boardOwner,
-    seed,
-  };
-
-  const metadataUrl = await IpfsUpload.uploadMetadata(ipfsMetadata);
-
-  const apiPayload = {
-    chainId: args.chainId,
-    signer: solanaWrapper.publicKey,
-    data: {
-      isSandboxMode: data.isSandboxMode,
-      seed,
-      advanced: true,
-      package: getLaunchPackageIndex(data.package),
-      token: {
-        name: token.name,
-        symbol: token.symbol,
-        totalSupply: token.totalSupply,
-        decimals: SOLANA_DECIMALS,
-        initialBuyAmount: token.initialBuyAmount,
-        metadataUrl,
-        raiseTokenDecimals: SOLANA_DECIMALS,
-      },
-      dex: {
-        routerId: dex.version,
-        ...(dex.version === SolanaDexType.METEORA && {
-          meteoraFeeTierIndex: dex.feeTier,
-        }),
-        ...(dex.version === SolanaDexType.RAYDIUM && {
-          raydiumFeeTierIndex: dex.feeTier,
-        }),
-      },
-      sale: {
-        marketCap: sale.marketCap,
-        softCap: sale.softCap,
-        endTime: sale.endTime,
-        initialBuyAmount: token.initialBuyAmount,
-        startTime: sale.startTime,
-        maxAllocationPerUser: sale.maxAllocationPerUser,
-        baseTokenForPair: SOLANA_BASE_TOKEN_PAIR,
-        baseTokenDecimals: SOLANA_DECIMALS,
-        referrer: sale.referrer,
-      },
-      ...(fees && {
-        fees: {
-          buyTokenOwnerFee: fees.buyPoolCreator,
-          sellTokenOwnerFee: fees.sellPoolCreator,
-          buyReferralFee: fees.buyReferral,
-          graduation: fees.graduation,
-        },
-      }),
-    },
-  };
-
-  const validated = createLbpSolanaApiPayloadSchema.parse(apiPayload);
-
-  const json = await BasedBidApi.invokeApi<CreateLbpSolanaApiResponse>(
-    ApiType.SDK,
-    'sol/create-lbp',
-    validated,
-    'Failed to create LBP on Solana',
-    args.isSandboxMode,
-  );
-
-  const { transaction, mintSignerSecretHex, blockhash, lastValidBlockHeight } =
-    json;
-
-  const mintSigner =
-    await solanaWrapper.getSignerFromPrivateKey(mintSignerSecretHex);
-
-  const signature = await solanaWrapper.sendTransaction(
-    transaction,
-    blockhash,
-    lastValidBlockHeight,
-    [mintSigner.keyPair],
-    { description: 'Create LBP', skipConfirmation: args.isSandboxMode },
-  );
-
-  await solanaWrapper.awaitTxConfirmation(signature);
-
-  if (!fees?.feeDistribution) {
-    return;
-  }
-
-  // Call fee-distribution API to set Fee Builder params
-
-  const feeDistributionPayload = {
-    chainId: args.chainId,
-    address: json.mintAddress,
-    ...fees,
-  };
-
-  const solanaFeeDistributionValidated =
-    solanaFeeDistributionApiPayloadSchema.safeParse(feeDistributionPayload);
-  if (!solanaFeeDistributionValidated.success) {
-    throw new Error(
-      'Invalid fee distribution payload: ' +
-        solanaFeeDistributionValidated.error.message,
+    const solanaWrapper = new SolanaWrapper(
+      env.SOLANA_RPC_URL,
+      env.SOLANA_PRIVATE_KEY,
     );
-  }
+    await solanaWrapper.init();
 
-  await BasedBidApi.invokeApi(
-    ApiType.PLATFORM,
-    'token/fee-distribution',
-    solanaFeeDistributionValidated.data,
-    'Failed to set fee distribution on Solana',
-    args.isSandboxMode,
-  );
+    const data = input;
+    const { token, board, boardOwner, dex, fees } = data;
+
+    let sale = data.sale;
+
+    if (!sale) {
+      sale = {
+        marketCap: '9000',
+        maxAllocationPerUser: '0',
+        whitelistedAddresses: [],
+      };
+    }
+
+    const logoUrl = await IpfsUpload.uploadImage(token.metadata.logo);
+
+    const seed = SeedGenerator.generateNumericSeed(5);
+
+    const ipfsMetadata = {
+      name: token.name,
+      symbol: token.symbol,
+      decimals: SOLANA_DECIMALS,
+      totalSupply: token.totalSupply,
+      logo: logoUrl,
+      twitter: token.metadata.twitter,
+      telegram: token.metadata.telegram,
+      website: token.metadata.website,
+      discord: token.metadata.discord,
+      description: token.metadata.description,
+      whitelist: sale.whitelistedAddresses,
+      board,
+      boardOwner,
+      seed,
+    };
+
+    const metadataUrl = await IpfsUpload.uploadMetadata(ipfsMetadata);
+
+    const apiPayload = {
+      chainId: args.chainId,
+      signer: solanaWrapper.publicKey,
+      data: {
+        isSandboxMode: data.isSandboxMode,
+        seed,
+        advanced: true,
+        package: getLaunchPackageIndex(data.package),
+        token: {
+          name: token.name,
+          symbol: token.symbol,
+          totalSupply: token.totalSupply,
+          decimals: SOLANA_DECIMALS,
+          initialBuyAmount: token.initialBuyAmount,
+          metadataUrl,
+          raiseTokenDecimals: SOLANA_DECIMALS,
+        },
+        dex: {
+          routerId: dex.version,
+          ...(dex.version === SolanaDexType.METEORA && {
+            meteoraFeeTierIndex: dex.feeTier,
+          }),
+          ...(dex.version === SolanaDexType.RAYDIUM && {
+            raydiumFeeTierIndex: dex.feeTier,
+          }),
+        },
+        sale: {
+          marketCap: sale.marketCap,
+          softCap: sale.softCap,
+          endTime: sale.endTime,
+          initialBuyAmount: token.initialBuyAmount,
+          startTime: sale.startTime,
+          maxAllocationPerUser: sale.maxAllocationPerUser,
+          baseTokenForPair: SOLANA_BASE_TOKEN_PAIR,
+          baseTokenDecimals: SOLANA_DECIMALS,
+          referrer: sale.referrer,
+        },
+        ...(fees && {
+          fees: {
+            buyTokenOwnerFee: fees.buyPoolCreator,
+            sellTokenOwnerFee: fees.sellPoolCreator,
+            buyReferralFee: fees.buyReferral,
+            graduation: fees.graduation,
+          },
+        }),
+      },
+    };
+
+    const validated = createLbpSolanaApiPayloadSchema.parse(apiPayload);
+
+    const json = await BasedBidApi.invokeApi<CreateLbpSolanaApiResponse>(
+      ApiType.SDK,
+      'sol/create-lbp',
+      validated,
+      'Failed to create LBP on Solana',
+      args.isSandboxMode,
+    );
+
+    const {
+      transaction,
+      mintSignerSecretHex,
+      blockhash,
+      lastValidBlockHeight,
+    } = json;
+
+    const mintSigner =
+      await solanaWrapper.getSignerFromPrivateKey(mintSignerSecretHex);
+
+    const signature = await solanaWrapper.sendTransaction(
+      transaction,
+      blockhash,
+      lastValidBlockHeight,
+      [mintSigner.keyPair],
+      { description: 'Create LBP', skipConfirmation: args.isSandboxMode },
+    );
+
+    launchedToken = {
+      chainId: args.chainId,
+      mintAddress: json.mintAddress,
+      signature,
+    };
+
+    await solanaWrapper.awaitTxConfirmation(signature);
+
+    if (!fees?.feeDistribution) {
+      return;
+    }
+
+    // Call fee-distribution API to set Fee Builder params
+
+    const feeDistributionPayload = {
+      chainId: args.chainId,
+      address: json.mintAddress,
+      ...fees,
+    };
+
+    const solanaFeeDistributionValidated =
+      solanaFeeDistributionApiPayloadSchema.safeParse(feeDistributionPayload);
+    if (!solanaFeeDistributionValidated.success) {
+      throw new Error(
+        'Invalid fee distribution payload: ' +
+          solanaFeeDistributionValidated.error.message,
+      );
+    }
+
+    await BasedBidApi.invokeApi(
+      ApiType.PLATFORM,
+      'token/fee-distribution',
+      solanaFeeDistributionValidated.data,
+      'Failed to set fee distribution on Solana',
+      args.isSandboxMode,
+    );
+
+    await BasedBidApi.invokeApi(
+      ApiType.SDK,
+      'sol/confirm-launch',
+      {
+        chainId: args.chainId,
+        mintAddress: json.mintAddress,
+        signature,
+      },
+      'Failed to confirm launch',
+      args.isSandboxMode,
+    );
+  } catch (error) {
+    console.error('Error creating LBP on Solana: ', error);
+    if (launchedToken != null) {
+      await BasedBidApi.invokeApi(
+        ApiType.SDK,
+        'sol/release-vanity',
+        {
+          chainId: launchedToken.chainId,
+          mintAddress: launchedToken.mintAddress,
+          signature: launchedToken.signature,
+        },
+        'Failed to release vanity',
+        args.isSandboxMode,
+      );
+    }
+    throw error;
+  }
 };
